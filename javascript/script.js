9b90af04effe2c313cce8d1798fefbdbedf30be3
Jhoão <128644543+Jhopn@users.noreply.github.com>
2023-12-15 23:32:41 -0300

    att

--- javascript/script.js
+++ javascript/script.js
@@ -1,98 +1,157 @@
 let botao = document.getElementById("quadrado");
 let maca = document.createElement("img");
+let div = document.getElementById("cobra")
+let audio = document.getElementById("musicaFundo");
+let controlaMusica = document.getElementById("controlaMusica");
+
+let exec
+function controlAudio(){
+  if(exec){
+    audio.play();
+    exec = false
+  }
+  else{
+    audio.pause();
+    exec = true
+  }
+}
+controlaMusica.addEventListener("click", controlAudio, exec)
+
 
 let vertical = 0
 let horizontal = 0
-
 let pontuacao = 0
 
+var windowWidth = window.innerWidth; // Altura da janela
+var windowHeight = window.innerHeight; // Largura da janela
+
+let valoresAlt = [];
+let valoresLarg = [];
+
+for (var i = 0; i < 350 ; i += 50) {
+  valoresAlt.push(i);
+}
+
+for (var i = 0; i <= windowHeight ; i += 50) {
+  valoresLarg.push(i);
+}
+console.log(valoresAlt)
+console.log(valoresLarg)
+
+
+// Cria a primeira ração
 maca.setAttribute("class", "maca");
 maca.src = "./imagens/images.jpg"
-document.body.appendChild(maca)
-maca.style.top = Math.round(Math.random() * ( 400 - 200) + 50) + "px"
-maca.style.left = Math.round(Math.random() * ( 400 - 200) + 50) + "px"
+div.appendChild(maca)
+let valorAlt = Math.floor(Math.random() * valoresAlt.length);
+let valorLarg = Math.floor(Math.random() * valoresLarg.length);
+maca.style.top = valoresAlt[valorAlt] + "px";
+maca.style.left = valoresLarg[valorLarg] + "px";
 
 document.addEventListener("keypress", (event)=>{
-    if (estaoSobrepostos(botao, maca)) {
-
-        pontuacao++
-        let valor = document.getElementById("valor");
-        valor.textContent = pontuacao
-
-        botao.style.width = (80 + pontuacao * 2) +"px";
-        botao.style.height = (80 + pontuacao * 2) +"px";
-
-        maca.remove()
-        maca.setAttribute("class", "maca");
-        maca.src = "./imagens/images.jpg"
-        document.body.appendChild(maca)
-        maca.style.top = Math.random() * ( 400 - 200) + 10 + "px"
-        maca.style.left = Math.random() * ( 400 - 200) + 10 + "px"
-      } else {
-        console.log('Os elementos não estão sobrepostos.');
-      }
 
     if(event.key == "w" || event.key == "PgUp"){
-        vertical = vertical - 50
-        botao.style.top = vertical + "px"
-
+        if( vertical <= 0){
+            botao.style.top = vertical + "px"
+        }
+        else{
+          vertical = vertical - 50
+          botao.style.top = vertical + "px"
+        }
     }
     else if(event.key == "a" || event.key == "Home"){
+      if(horizontal <= 0){
+        botao.style.top = vertical + "px"
+      }
+      else{
         horizontal = horizontal - 50
         botao.style.left = horizontal + "px";
+      }
     }
     else if(event.key == "s"){
+      if( vertical >= (windowHeight - 270)){
+        botao.style.top = vertical + "px";
+      } else {
         vertical = vertical + 50
         botao.style.top = vertical + "px";
-
+      }
     }
     else if(event.key == "d"){
+      if(horizontal >= (windowWidth - 130)){
+        botao.style.left = horizontal + "px"
+
+      } else{
         horizontal = horizontal + 50
         botao.style.left = horizontal + "px"
+
+      }
     }
 
 });
 
 
-    function estaoSobrepostos(player, maca) {
-        const imgPlayer = player.getBoundingClientRect();
-        const macaImg = maca.getBoundingClientRect();
+function estaoSobrepostos(player, maca) {
+  const imgPlayer = player.getBoundingClientRect();
+  const macaImg = maca.getBoundingClientRect();
 
-        let horizon = Math.floor(imgPlayer.left * 8);
-        let horizonTwo = Math.floor(macaImg.left * 8);
-        horizon = horizon.toString();
-        horizonTwo = horizonTwo.toString();
-        horizon = horizon.slice(0, 2);
-        horizonTwo = horizonTwo.slice(0, 2);
-
-
-
-        let vertical = Math.floor(imgPlayer.top * 8);
-        let verticalTwo = Math.floor(macaImg.top * 8);
-        vertical = vertical.toString();
-        verticalTwo = verticalTwo.toString();
-        vertical = vertical.slice(0, 2);
-        verticalTwo = verticalTwo.slice(0, 2);
+  let horizon = Math.floor((imgPlayer.left * 8)/100);
+  let horizonTwo = Math.floor((macaImg.left * 8)/100);
+  horizon = horizon.toString();
+  horizonTwo = horizonTwo.toString();
+  horizon = parseInt(horizon);
+  horizonTwo = parseInt(horizonTwo);
 
 
-        console.log(horizon);
-        console.log(horizonTwo);
-        console.log(vertical);
-        console.log(verticalTwo);
 
-        let diferencaHorin = horizon -  horizonTwo
-        let diferencaVer = vertical - verticalTwo
-  
-        return (
-          diferencaHorin < 3  &&
-          diferencaVer < 3
-        );
-      }
+  let vertical = Math.floor((imgPlayer.top * 8)/100);
+  let verticalTwo = Math.floor((macaImg.top * 8)/100);
+  vertical = vertical.toString();
+  verticalTwo = verticalTwo.toString();
+  vertical = parseInt(vertical);
+  verticalTwo = parseInt(verticalTwo);
+
+  // console.log(horizon);
+  // console.log(horizonTwo);
+  // console.log(vertical);
+  // console.log(verticalTwo);
+
+  let diferencaHorin = horizonTwo - horizon
+  let diferencaVer = verticalTwo - vertical
+
+  console.log(diferencaHorin)
+  console.log(diferencaVer)
+
+  return (
+    diferencaHorin <= 5  &&
+    diferencaVer <= 5 &&
+    diferencaHorin - diferencaVer <= 3
+  );
+}
 
 document.addEventListener("keypress", (event)=>{
+  if (estaoSobrepostos(botao, maca)) {
+
+    pontuacao++
+    let valor = document.getElementById("valor");
+    valor.textContent = pontuacao
+
+    // botao.style.width = (80 + pontuacao * 2) +"px";
+    // botao.style.height = (80 + pontuacao * 2) +"px";
+
+    maca.setAttribute("class", "maca");
+    maca.src = "./imagens/images.jpg"
+    div.appendChild(maca)
+    let valorAlt = Math.floor(Math.random() * valoresAlt.length);
+    let valorLarg = Math.floor(Math.random() * valoresLarg.length);
+    maca.style.top = valoresAlt[valorAlt] + "px";
+    maca.style.left = valoresLarg[valorLarg] + "px";
+  } else {
+    console.log('Os elementos não estão sobrepostos.');
+  }
+
   if( event.key == " "){
     document.body.style.backgroundColor = "rgba(49, 129, 250, 0.795)";
-    botao.src = "./imagens/gatoPidao2.jpg"
+    botao.src = "./imagens/gato2.png"
   }
 });
 
