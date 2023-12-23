6a588a4d59480101b8ca7fb8309d045e1acdcc35
Jhoão <128644543+Jhopn@users.noreply.github.com>
2023-12-23 11:32:38 -0300

    att

--- javascript/script.js
+++ javascript/script.js
@@ -39,7 +39,7 @@ for (var i = 0; i < 350 ; i += 50) {
   valoresAlt.push(i);
 }
 
-for (var i = 0; i <= windowHeight - 350 ; i += 50) {
+for (var i = 0; i <= windowHeight - 400 ; i += 50) {
   valoresLarg.push(i);
 }
 console.log(valoresAlt)
