797587a30ce3d17ee97af891120da71b8fe6afe4
Jhoão <128644543+Jhopn@users.noreply.github.com>
2024-01-02 11:45:44 -0300

    att

--- javascript/script.js
+++ javascript/script.js
@@ -7,24 +7,6 @@ let path = document.getElementById("path");
 let setasControla = document.getElementById("setasControla");
 
 
-let exec
-function controlAudio(){
-  if(exec){
-    audio.play();
-    exec = false
-    controlaMusica.src = "./imagens/volume-down-fill.svg"
-  }
-  else{
-    audio.pause();
-    exec = true
-    controlaMusica.src = "./imagens/volume-mute-fill.svg"
-
-  }
-}
-
-controlaMusica.addEventListener("click", controlAudio, exec)
-
-
 let vertical = 0
 let horizontal = 0
 let pontuacao = 0
@@ -39,7 +21,7 @@ for (var i = 0; i < 350 ; i += 50) {
   valoresAlt.push(i);
 }
 
-for (var i = 0; i <= windowHeight - 550 ; i += 50) {
+for (var i = 0; i <= windowHeight - 100 ; i += 50) {
   valoresLarg.push(i);
 }
 console.log(valoresAlt);
@@ -95,44 +77,8 @@ document.addEventListener("keypress", (event)=>{
 });
 
 
-function estaoSobrepostos(player, maca) {
-  const imgPlayer = player.getBoundingClientRect();
-  const macaImg = maca.getBoundingClientRect();
 
-  let horizon = Math.floor((imgPlayer.left * 8)/100);
-  let horizonTwo = Math.floor((macaImg.left * 8)/100);
-  horizon = horizon.toString();
-  horizonTwo = horizonTwo.toString();
-  horizon = parseInt(horizon);
-  horizonTwo = parseInt(horizonTwo);
-
-
-
-  let vertical = Math.floor((imgPlayer.top * 8)/100);
-  let verticalTwo = Math.floor((macaImg.top * 8)/100);
-  vertical = vertical.toString();
-  verticalTwo = verticalTwo.toString();
-  vertical = parseInt(vertical);
-  verticalTwo = parseInt(verticalTwo);
-
-  // console.log(horizon);
-  // console.log(horizonTwo);
-  // console.log(vertical);
-  // console.log(verticalTwo);
-
-  let diferencaHorin = horizonTwo - horizon
-  let diferencaVer = verticalTwo - vertical
-
-  console.log(diferencaHorin)
-  console.log(diferencaVer)
-
-  return (
-    diferencaHorin <= 5  &&
-    diferencaVer <= 5 &&
-    diferencaHorin - diferencaVer <= 3
-  );
-}
-
+// Verifica se estão sobre um o outro
 document.addEventListener("keypress", (event)=>{
   if (estaoSobrepostos(botao, maca)) {
 
@@ -155,18 +101,11 @@ document.addEventListener("keypress", (event)=>{
   }
 });
 
-function obterCoordenadas(event) {
-  var x = event.touches[0].clientX;
-  var y = event.touches[0].clientY;
 
+// Move a Div atráves do toque na tela
+document.addEventListener('touchstart', moveDiv);
 
-  botao.style.top = y + "px";
-  botao.style.left = x + "px";
-
-}
-
-document.addEventListener('touchstart', obterCoordenadas);
-
+// Verifica se estão sobre um o outro
 document.addEventListener("touchstart", (event)=>{
   if (estaoSobrepostos(botao, maca)) {
 
@@ -191,73 +130,34 @@ document.addEventListener("touchstart", (event)=>{
 });
 
 
-
+// Tela Cheia
 isFullScreen = false;
 var elem = document.documentElement;
-function AtivarDesativarFS() {
-    //Se o estado atual for "FullScreen", desativá-lo.
-    //Note que para as verificações é feito uma validação para todos os possíveis navegadores, facilitando a sua vida.
-      if (document.exitFullscreen) {
-      document.exitFullscreen();
-      isFullScreen = false;
-    } else if (document.mozCancelFullScreen) { /* Firefox */
-      document.mozCancelFullScreen();
-      isFullScreen = false;
-    } else if (document.webkitExitFullscreen) { /* Chrome, Safari & Opera */
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { /* IE/Edge */
-      document.msExitFullscreen();
-      isFullScreen = false;
-    }
-
-
-  if (elem.requestFullscreen) {
-     elem.requestFullscreen();
-     isFullScreen = true;
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-     elem.mozRequestFullScreen();
-      isFullScreen = true;
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-    elem.webkitRequestFullscreen();
-     isFullScreen = true;
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-     isFullScreen = true;
-  }
-
-  if(isFullScreen === true){
-    telaCheia.src = "./imagens/fullscreen-exit.svg";
-  } 
-
-}
-
 let telaCheia = document.getElementById("telaCheia");
 telaCheia.addEventListener("click", AtivarDesativarFS);
 
 
-
-function configuracoes(){
-  let menu = document.querySelector(".menu");
-  if( menu.classList.contains("aberto")){
-    menu.classList.remove("aberto");
-    imagemConf.src = "./imagens/gear-fill.svg";
-  } else {
-    menu.classList.add("aberto");
-    imagemConf.src = "./imagens/gear.svg";
-  }
-
-
-}
+// Configurações
 let opcoes = document.getElementById("configuracoes");
 let imagemConf = document.getElementById("imagemConfiguracoes");
 opcoes.addEventListener("click", configuracoes);
 
 
 // Temas
-
 let temas = document.getElementById("temas");
-
 let temaAtivo = false
+temas.addEventListener("click", temasCat, temaAtivo)
+
+
+// Controla o audio
+let exec
+controlaMusica.addEventListener("click", controlAudio, exec)
+
+
+
+
+// Funções
+// Temas
 function temasCat(){
   if( temaAtivo === false){
     document.body.style.backgroundColor = "rgba(49, 129, 250, 0.795)";
@@ -272,4 +172,113 @@ function temasCat(){
   
 }
 
-temas.addEventListener("click", temasCat, temaAtivo)
+// Configurações 
+function configuracoes(){
+  let menu = document.querySelector(".menu");
+  if( menu.classList.contains("aberto")){
+    menu.classList.remove("aberto");
+    imagemConf.src = "./imagens/gear-fill.svg";
+  } else {
+    menu.classList.add("aberto");
+    imagemConf.src = "./imagens/gear.svg";
+  }
+
+}
+
+//Tela Cheia
+function AtivarDesativarFS() {
+  //Se o estado atual for "FullScreen", desativá-lo.
+  //Note que para as verificações é feito uma validação para todos os possíveis navegadores, facilitando a sua vida.
+    if (document.exitFullscreen) {
+    document.exitFullscreen();
+    isFullScreen = false;
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+    isFullScreen = false;
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari & Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+    isFullScreen = false;
+  }
+
+
+if (elem.requestFullscreen) {
+   elem.requestFullscreen();
+   isFullScreen = true;
+} else if (elem.mozRequestFullScreen) { /* Firefox */
+   elem.mozRequestFullScreen();
+    isFullScreen = true;
+} else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+  elem.webkitRequestFullscreen();
+   isFullScreen = true;
+} else if (elem.msRequestFullscreen) { /* IE/Edge */
+  elem.msRequestFullscreen();
+   isFullScreen = true;
+}
+
+if(isFullScreen === true){
+  telaCheia.src = "./imagens/fullscreen-exit.svg";
+} 
+
+}
+
+// Move a Div na tela
+function moveDiv(event) {
+  var x = event.touches[0].clientX;
+  var y = event.touches[0].clientY;
+
+  botao.style.top = Math.floor(y) + "px";
+  botao.style.left = Math.floor(x) + "px";
+
+}
+
+// Verifica se os elementos estão um sobre o outro
+function estaoSobrepostos(player, maca) {
+  const imgPlayer = player.getBoundingClientRect();
+  const macaImg = maca.getBoundingClientRect();
+
+  let horizon = Math.floor((imgPlayer.left * 8)/100);
+  let horizonTwo = Math.floor((macaImg.left * 8)/100);
+  horizon = horizon.toString();
+  horizonTwo = horizonTwo.toString();
+  horizon = parseInt(horizon);
+  horizonTwo = parseInt(horizonTwo);
+
+
+
+  let vertical = Math.floor((imgPlayer.top * 8)/100);
+  let verticalTwo = Math.floor((macaImg.top * 8)/100);
+  vertical = vertical.toString();
+  verticalTwo = verticalTwo.toString();
+  vertical = parseInt(vertical);
+  verticalTwo = parseInt(verticalTwo);
+
+
+  let diferencaHorin = horizonTwo - horizon
+  let diferencaVer = verticalTwo - vertical
+
+  console.log(diferencaHorin)
+  console.log(diferencaVer)
+
+  return (
+    diferencaHorin <= 3  &&
+    diferencaVer <= 3 &&
+    diferencaHorin - diferencaVer <= 3
+  );
+}
+
+// Controla o audio
+function controlAudio(){
+  if(exec){
+    audio.play();
+    exec = false
+    controlaMusica.src = "./imagens/volume-down-fill.svg"
+  }
+  else{
+    audio.pause();
+    exec = true
+    controlaMusica.src = "./imagens/volume-mute-fill.svg"
+
+  }
+}
